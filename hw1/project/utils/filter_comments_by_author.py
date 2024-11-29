e929cbecb47f417866dfa4b8d2fbcdfe369e2f1a
Nikita <72099127+NikitaRadionov@users.noreply.github.com>
2024-11-29 21:58:51 +0300

    Delete hw1/project directory

--- hw1/project/utils/filter_comments_by_author.py
+++
@@ -1,2 +0,0 @@
-def filter_comments_by_author(comments: list, author: object) -> list:
-    return list(filter(lambda comment: comment.author_id == author.id, comments))
--- hw1/project/utils/select_products_by_category.py
+++
@@ -1,2 +0,0 @@
-def select_products_by_category(products: list, category: str) -> list:
-    return list(filter(lambda product: product.category == category, products))
